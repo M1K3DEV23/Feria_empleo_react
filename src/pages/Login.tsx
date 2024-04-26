2d2526fc086881809a9445bc9b21e641f4b7f09c
M1K3DEV23 <mikery2310@outlook.com>
2024-04-26 17:55:35 -0600

    agregando la api

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -28,7 +28,7 @@ const Login: React.FC = () => {
     event.preventDefault();
 
     try {
-      const response = await fetch('', {
+      const response = await fetch('http://127.0.0.1:8000/api/login', {
         method: "POST",
         headers: {
           'Content-Type': 'application/json',
